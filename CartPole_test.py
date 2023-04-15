9dfc7313e66f8d4b7082315e1234c64e27cb6c12
Desperat1on <ppf0912@163.com>
2023-04-15 09:35:37 +0800

    add some annotations

--- CartPole_test.py
+++ CartPole_test.py
@@ -1,5 +1,5 @@
 import gym
 
-env = gym.make('CartPole-v1', render_mode="human")  # 指定render_mode为human以显示画面
+env = gym.make('CartPole-v1', render_mode="human")  # v0已过时，指定render_mode为human以显示画面
 env.reset()
 env.render()
